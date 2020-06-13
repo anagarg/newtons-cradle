e35e44e26eb7a4173cd3cb2f745228061e92a3c4
anagarg <64941411+anagarg@users.noreply.github.com>
2020-06-13 00:18:41 -0700

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,17 @@
+class Roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(World, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("grey");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  }
